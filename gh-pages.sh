2c5b6f1e1b84d7b79fbe6ef335569c0a8b2db308
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-06-29 08:30:18 -0500

    gh-pages support
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git config --global user.email "gkt@cs.luc.edu"
+git config --global user.name "George K. Thiruvathukal"
+git remote add deploy "https://$GH_TOKEN@github.com/$GH_USER/$GH_REPO.git"
+git fetch deploy
+git reset deploy/gh-pages
+git checkout master source Makefile build-handbook.sh
+rm -rf build
+./build-handbook.sh
+mkdir -p download
+mv -fv build/html/* ./
+mv -fv build/latex/*.pdf ./download/
+mv -fv build/epub/*.epub ./download/
+touch .nojekyll
+git add -A
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push deploy HEAD:gh-pages
+
