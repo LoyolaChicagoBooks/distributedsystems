8521b8fa7dc02ba4094f6cf8aea2c5475318dc3c
George K. Thiruvathukal <gkt@cs.luc.edu>
2019-03-15 21:57:38 -0500

    use git describe for latest tag

--- get-current-version.sh
+++ get-current-version.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# This returns the last tag. $1 is the value to return if there are no tags.
-
 IF_MISSING=$1
 NUMBER_OF_TAGS=$(git tag | wc -l)
 if [ $NUMBER_OF_TAGS -gt 0 ]; then
-  echo $(git tag | tail -n1)
+  echo $(git describe --tags --abbrev=0 )
 else
   echo "$IF_MISSING"
 fi
