128c93c0d073159781ddeb1579c45da0cd1adc73
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-11-27 22:07:14 -0600

    fix code examples script -- again

--- bin/get-code-examples.sh
+++ bin/get-code-examples.sh
@@ -4,6 +4,6 @@ EXAMPLES=examples
 mkdir -p $EXAMPLES
 rm -rf $EXAMPLES/*
 
-git clone git@github.com:LoyolaChicagoCode/hpjpc-source-java.git $EXAMPLES/hpjpc
-git clone git@github.com:SoftwareSystemsLaboratory/systems-code-examples.git $EXAMPLES/systems-code-samples
-git clone git@github.com:gkthiruvathukal/multicast-java.git $EXAMPLES/multicast-java
+git clone https://github.com/LoyolaChicagoCode/hpjpc-source-java.git $EXAMPLES/hpjpc
+git clone https://github.com/SoftwareSystemsLaboratory/systems-code-examples.git $EXAMPLES/systems-code-examples
+git clone https://github.com/gkthiruvathukal/multicast-java.git $EXAMPLES/multicast-java
